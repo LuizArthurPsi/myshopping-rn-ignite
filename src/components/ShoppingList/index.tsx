9288b8789c5caba5a25a4a94c73bac1e801f42fe
Luiz Arthur <luizarthurvm@gmail.com>
2022-04-21 16:09:10 -0300

    Leitura única de documentos

--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FlatList } from 'react-native';
+
+import firestore from '@react-native-firebase/firestore';
 
 import { styles } from './styles';
 import { Product, ProductProps } from '../Product';
@@ -8,6 +10,24 @@ import { shoppingListExample } from '../../utils/shopping.list.data';
 
 export function ShoppingList() {
   const [products, setProducts] = useState<ProductProps[]>(shoppingListExample);
+
+  useEffect(() => {
+    firestore()
+    .collection('Products')
+    .get()
+    .then(response => {
+      const data = response.docs.map(doc => {
+        return {
+          id: doc.id,
+          ...doc.data()
+        }
+      }) as ProductProps[];
+
+      setProducts(data);
+    })
+    .catch(error => console.log(error));
+
+  },[])
 
   return (
     <FlatList
