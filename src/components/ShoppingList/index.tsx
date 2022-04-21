2a6812f462793d45ec2298f52e950cdf03b3af8e
Luiz Arthur <luizarthurvm@gmail.com>
2022-04-21 16:21:34 -0300

    Limitando consultas

--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -14,7 +14,7 @@ export function ShoppingList() {
   useEffect(() => {
     firestore()
     .collection('Products')
-    .where('quantity', '>', 1)
+    .limit(5)
     .onSnapshot(querySnapshot => {
       const data = querySnapshot.docs.map((doc) => {
         return {
